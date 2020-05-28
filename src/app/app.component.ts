2c3630f3538318330e0d36c82a590e93e5e4b102
tomriddle7 <tomriddle7@naver.com>
2020-05-28 14:00:09 +0900

    Create own todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-todo';
+  todos = [{
+    id: 1,
+    title: '요가 수행하기',
+    completed: false
+  }, {
+    id: 2,
+    title: '어머니 용돈 드리기',
+    completed: true
+  }];
+  checkoutForm;
+  statusFilter;
+  angularTodos = this.todos;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+    this.checkoutForm = this.formBuilder.group({
+      newTodo: ''
+    });
+  }
+
+  addTodo = function (todoTitle) {
+    todoTitle = todoTitle.newTodo.trim();
+    if (!todoTitle) return;
+
+    let newId = !this.todos.length ?
+        1 : this.todos[this.todos.length - 1].id + 1;
+
+    let newTodo = {
+      id: newId,
+      title: todoTitle,
+      completed: false
+    };
+
+    this.todos = this.todos.concat(newTodo);
+    this.angularTodos = this.todos;
+  };
+
+  removeTodo = function (id) {
+    if (!id) return;
+
+    // 배열에서 제거
+    this.todos = this.todos.filter(todo => todo.id != id);
+    this.angularTodos = this.todos;
+  }
+
+  filterTodo = function (filter: string) {
+    this.statusFilter = filter;
+    if(this.statusFilter === 'completed') {
+      this.angularTodos = this.todos.filter(todo => todo.completed === true);
+    } else if(this.statusFilter === 'active') {
+      this.angularTodos = this.todos.filter(todo => todo.completed === false);
+    } else {
+      this.angularTodos = this.todos;
+    }
+  }
+
+  clearCompleted = function() {
+    let incompleteTodos = this.todos.filter(todo => todo.completed === false);
+    this.todos = incompleteTodos;
+    this.angularTodos = this.todos;
+  }
 }
